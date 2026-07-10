a59575b7514859dcfa9341850b78dbd3d8846964
Beulah Anisha <beulahanishaj@gmail.com>
2026-07-11 01:02:17 +0530

    Connect PostgreSQL and create users table

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,10 +1,16 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
     APP_NAME: str = "IntelliFlow AI"
     APP_VERSION: str = "1.0.0"
     DEBUG: bool = True
+    DATABASE_URL: str
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8"
+    )
 
 
 settings = Settings()
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,24 @@
 from fastapi import FastAPI
+
+from app.models import user
+
 from app.core.config import settings
+from app.core.database import Base, engine
+
+from app.models.user import User
+
 from app.api.health import router as health_router
 from app.api.users import router as user_router
+
 
 app = FastAPI(
     title=settings.APP_NAME,
     version=settings.APP_VERSION
 )
+
+
+Base.metadata.create_all(bind=engine)
+
 
 app.include_router(health_router)
 app.include_router(user_router)
